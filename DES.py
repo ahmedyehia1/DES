b148fd907119697a35826676e15bcf447dac38d8
Ahmed Yehia <ahmedyehia.edu@gmail.com>
2021-02-01 19:45:05 +0200

    Implement Decryption functionality

--- DES.py
+++ DES.py
@@ -9,18 +9,13 @@ def show(mang,w=8):
         print(mang[i:i+w],sep='\n')
 
 class DES:
-    def __propagation(self,plaintext,key):
+    def __propagation(self,plaintext,roundKeys):
         roundtext = plaintext.copy()
         for i,p in enumerate(IP):
             roundtext[i] = plaintext[p-1]
-        Pkey = [0]*56
-        for i,p in enumerate(PC1):
-            Pkey[i] = key[p-1]
-        lk , rk =  Pkey[:28],Pkey[28:]
         left , right = roundtext[:32] , roundtext[32:]
         for r in range(16):
-            lk , rk , roundKey = keyGen(lk,rk,SHIFT[r])
-            mang = Mangler(right,roundKey)
+            mang = Mangler(right,roundKeys[r])
             left , right = right , [l ^ m for l,m in zip(left,mang)]
         roundtext = right + left
         for i,p in enumerate(IP_1):
@@ -30,15 +25,33 @@ class DES:
     def Encrypt(self,plaintext,key):
         plaintext = ToBin(plaintext)
         key = ToBin(key)
-        return hex(ToInt(self.__propagation(plaintext,key)))[2:].upper()
+        roundKeys = []
+        Pkey = [0]*56
+        for i,p in enumerate(PC1):
+            Pkey[i] = key[p-1]
+        lk , rk =  Pkey[:28],Pkey[28:]
+        for r in range(16):
+            lk , rk , roundKey = keyGen(lk,rk,SHIFT[r])
+            roundKeys.insert(-1,roundKey)
+        return hex(ToInt(self.__propagation(plaintext,roundKeys)))[2:].upper()
 
     def Decrypt(self,ciphertext,key):
-        pass
+        ciphertext = ToBin(ciphertext)
+        key = ToBin(key)
+        roundKeys = []
+        Pkey = [0]*56
+        for i,p in enumerate(PC1):
+            Pkey[i] = key[p-1]
+        lk , rk =  Pkey[:28],Pkey[28:]
+        for r in range(16):
+            lk , rk , roundKey = keyGen(lk,rk,SHIFT[r])
+            roundKeys.insert(0,roundKey)
+        return hex(ToInt(self.__propagation(ciphertext,roundKeys)))[2:].upper()
 
 cipher = DES()
-plaintext = "FFFFFFFFFFFFFFFF"
+plaintext = "abcdef1221abcdef"
 key = "0000000000000000"
-d = cipher.Encrypt(plaintext,key)
-print(d)
-d = cipher.Encrypt(d,key)
-print(d)
+ciphertext = cipher.Encrypt(plaintext,key)
+print(ciphertext)
+plaintext = cipher.Decrypt(ciphertext,key)
+print(plaintext)
